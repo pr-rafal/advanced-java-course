10546841d092bac6e0ddae61a90debb55d8afa2b
pr-rafal <rafal.prusko@gmail.com>
2023-08-03 17:01:23 +0200

    Created

---
+++ guava-example/src/main/java/com/caveofprograming/examples/guava_example/App.java
@@ -0,0 +1,22 @@
+package com.caveofprograming.examples.guava_example;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		
+		Multiset<String> animals = HashMultiset.create();
+		
+		animals.add("cat");
+		animals.add("cat");
+		animals.add("dog");
+		
+		animals.forEach(System.out::println);
+		
+	}
+}
